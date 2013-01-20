e2406b95e842c257ba98da382d6c290076e39327
JasonFruit <JasonFruit@hushmail.com>
2013-01-20 14:00:42 -0800

    Commented stuff.

--- clips.py
+++ clips.py
@@ -3,25 +3,35 @@ import cPickle
 __author__ = 'jason'
 
 class ClipManager(object):
+    """Represents a categorized collection of clips"""
     def __init__(self):
         self.categories = {"unclassified": []}
 
     def add(self, clip, category="unclassified"):
+        """Add a clip in the specified category, or 'unclassified' if
+        none is specified"""
         try:
             self.categories[category].append(clip)
         except KeyError:
             self.categories[category] = [clip]
 
     def add_category(self, category):
-        self.categories[category] = []
+        """Add a new empty category, if it doesn't already exist"""
+        if not category in self.categories.keys:
+            self.categories[category] = []
 
     def remove_category(self, category):
+        """Remove the specified category"""
         self.categories.remove(category)
 
     def delete(self, clip, category="unclassified"):
+        """Remove the specified clip from the specified category (the
+        same clip can exist in multiple categories)"""
         self.categories[category].remove(clip)
 
     def move(self, clip, from_category, to_category):
+        """Move a clip from one category to another"""
+        
         # remove the clip from the old category
         self.categories[from_category].remove(clip)
 
@@ -34,14 +44,17 @@ class ClipManager(object):
 
 
 class Clip(object):
+    """Represents a titled clip of HTML scripture"""
     def __init__(self, title, html):
         self.title = title
         self.html = html
 
 
 def save_to_file(filename, manager):
+    """Save the specified ClipManager to the specified filename"""
     cPickle.dump(manager, open(filename, "wb"))
 
 
 def load_from_file(filename):
+    """Load a clip manager from the specified filename"""
     return cPickle.load(open(filename, "rb"))
--- rc.py
+++ rc.py
@@ -3,20 +3,26 @@ __author__ = 'jason'
 import os
 from PySide.QtCore import QDir
 
+# the rc that is used if none exists or the current one is otherwise
+# unrecoverable
 default_rc = {"font": "sans",
               "version": "esv",
               "init_passage": "John 3"}
 
 def rc_dir():
+    """Return the directory that contains the rc files"""
     return os.path.join(QDir.home().absolutePath(), ".biblereader")
 
 def rc():
+    """Return the path to the main rc file"""
     return os.path.join(rc_dir(), "rc")
 
 def clip_rc():
+    """Return the path to the clip rc file"""
     return os.path.join(rc_dir(), "clips")
 
 def rc_exists():
+    """Check if all the elements of the rc directory exist"""
     return (os.path.isdir(rc_dir()) and
             os.path.exists(rc()) and
             os.path.exists(clip_rc()))
